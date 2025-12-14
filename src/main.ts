4ed92acc8a30ab4e858e4783f9d4864a1942ee35
cbdyzj <cbdyzj@gmail.com>
2025-12-14 21:17:15 +0800

    feat: add initial HTML template and static file serving in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { Hono } from 'hono'
 import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
 
 const app = new Hono()
+
+app.use('/*', serveStatic({ root: './public' }))
 
 app.get('/', (c) => {
   return c.text('Hono!')
